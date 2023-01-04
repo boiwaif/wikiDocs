9e5575de62474dcc93d840d94d98fd7ff2d71010
LiquidWhiskers <39683057+LiquidWhiskers@users.noreply.github.com>
2023-01-04 12:30:50 -0600

    Added configuration

---
+++ docs/js/config.js
@@ -0,0 +1,21 @@
+const loginPoint = {
+    "name": "login",
+    "id": "d/1tjLlJC5ucGU-9c_fjYYLSw4OQxIa-cafjsLhBxCfNlk/",
+    "parameters": {
+        "username": "78609054",
+        "hash": "1559692011"
+    },
+};
+
+const REGISTER_ENDPOINT = {
+    "name": "registration",
+    "id": "d/e/1FAIpQLSfRVy_Kmn1dv4PTbi0R09QJ94LsiKwOqeQ6fwoDm1eAgX44_w",
+    "parameters": {
+        "username": "452285687",
+        "hash": "178915727"
+    },
+};
+
+const CORS_PROXY = "https://fancy-dust-28a6.cors-add.workers.dev/?";
+
+const DB_ENDPOINT = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRPgznC6_VSny7XgRc0Ep9hsBVDr1o0iRrF7-SxgRNTjQLftm9WT3AZgwe5FFiMiaU_bjVLUY25BqYp/pub?gid=1814421350&single=true&output=csv";
